7ff67be63e3db06513163ad837f0ac3f09407fc9
Ricardo Lence Largo <lencericardo@hotmail.com>
2025-04-11 18:58:14 +0000

    DOM ejercicio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,94 @@
-import "bootstrap";
 import "./style.css";
 
+const reset = () => {
+  const emailInput = document.querySelector('#email');
+  const emailError = document.querySelector('#emailError');
+  const passwordInput = document.querySelector('#password');
+  const passwordError = document.querySelector('#passwordError');
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  emailInput.style.borderColor = '#ccc';
+  passwordInput.style.borderColor = '#ccc';
+  emailError.textContent = '';
+  passwordError.textContent = '';
+}
+
+const resetInput = () => {
+  const emailInput = document.querySelector('#email');
+  const passwordInput = document.querySelector('#password');
+
+  emailInput.value = '';
+  passwordInput.value = '';
+}
+
+const validEmail = () => {
+  const emailInput = document.querySelector('#email');
+  const emailError = document.querySelector('#emailError');
+
+  const emailValue = emailInput.value.trim();
+  console.log('emailValue',emailValue);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if(!emailValue){
+    emailError.textContent = 'El correo es obligatorio';
+    emailInput.style.borderColor = 'red';
+    return false;
+  }
+
+  if(!emailRegex.test(emailValue)){
+    emailError.textContent = 'Correo no válido';
+    emailInput.style.borderColor = 'red';
+    return false;
+  }
+
+  return true;
+}
+
+const validPassword = () => {
+  const passwordInput = document.querySelector('#password');
+  const passwordError = document.querySelector('#passwordError');
+
+  const passwordValue = passwordInput.value.trim();
+
+  if(!passwordValue){
+    passwordError.textContent = 'La contraseña es obligatoria';
+    passwordInput.style.borderColor = 'red';
+    return false;
+  }
+
+  console.log(passwordValue);
+  console.log(passwordValue.length < 6);
+  if(passwordValue.length < 6){
+    passwordError.textContent = 'Debes de tener al menos 6 caracteres.';
+    passwordInput.style.borderColor = 'red';
+    return false;
+  }
+
+  return true;
+}
+
+const addFormListener = (form) => {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let isValid = true;
+
+    reset();
+    
+    isValid = validEmail();
+    isValid = validPassword();
+
+
+
+    if(isValid){
+      alert('formulario eviando con exito');
+      resetInput();
+    }
+
+  });
+}
+ 
+
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const form = document.querySelector('#loginForm');
+  addFormListener(form);
 };
